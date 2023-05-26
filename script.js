29a7913798581ab7a6f15ad49dd31cb1d8ba2872
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-25 19:11:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var city = $('#city-input').val();
     if (city !== '') {
       var apiKey = '523db02a5fd9feaf907bb544946b23ef';
-      var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+      var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
       
       $.ajax({
         url: apiUrl,
@@ -21,16 +21,25 @@ $(document).ready(function() {
   });
 
   function displayWeatherInfo(data) {
-    var cityName = data.name;
-    var temperature = Math.round(data.main.temp - 273.15); // Convert to Celsius
-    var humidity = data.main.humidity;
-    var windSpeed = data.wind.speed;
+    var cityName = data.city.name;
+    var forecastList = data.list;
 
-    var info = '';
-    info += '<p><strong>City:</strong> ' + cityName + '</p>';
-    info += '<p><strong>Temperature:</strong> ' + temperature + '°C</p>';
-    info += '<p><strong>Humidity:</strong> ' + humidity + '%</p>';
-    info += '<p><strong>Wind Speed:</strong> ' + windSpeed + ' km/h</p>';
+    var info = '<h2>Weather Forecast for ' + cityName + '</h2>';
+    for (var i = 0; i < forecastList.length; i += 8) {
+      var forecastData = forecastList[i];
+      var forecastDate = new Date(forecastData.dt_txt);
+      var temperature = Math.round(forecastData.main.temp - 273.15); // Convert to Celsius
+      var humidity = forecastData.main.humidity;
+      var windSpeed = forecastData.wind.speed;
+
+      info += '<div class="forecast-item">';
+      info += '<h3>' + forecastDate.toDateString() + '</h3>';
+      info += '<p><strong>Temperature:</strong> ' + temperature + '°C</p>';
+      info += '<p><strong>Humidity:</strong> ' + humidity + '%</p>';
+      info += '<p><strong>Wind Speed:</strong> ' + windSpeed + ' km/h</p>';
+      info += '</div>';
+    }
+
     $('#weather-info').html(info);
   }
 });
